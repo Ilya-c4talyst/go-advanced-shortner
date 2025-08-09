e11bce3337091779f6cf8360998e98cba049228f
Ilya Savchenko <c4talyst12@yandex.ru>
2025-08-10 00:19:04 +0300

    iter4: Test launch2

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -8,7 +8,7 @@ import (
 // parseFlags обрабатывает аргументы командной строки
 func parseFlags() (string, string) {
 	// адрес запуска HTTP-сервера значением :8080 по умолчанию
-	portFlag := flag.String("a", "8080", "address and port to run server")
+	portFlag := flag.String("a", "localhost:8080", "address and port to run server")
 
 	// базовый адрес результирующего сокращённого URL значением
 	resAddressFlag := flag.String("b", "http://localhost:8080", "address and port for short url")
